2123a8b3c0ac5745643c734aa65a296ef61069a2
Alucard <xavier_ortega98@hotmail.com>
2021-08-13 12:50:17 -0500

    App funcionando faltan fixes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {GoogleOutlined, FacebookFilled} from '@ant-design/icons';
 import "firebase/app";
-
 import {auth} from '../firebase'
 import firebase from "firebase/app";
 
@@ -9,7 +8,7 @@ export const Login = () => {
     return (
         <div id="login-page">
             <div id="login-card">
-                <h2>Welcome to unichat</h2>
+                <h2> Bienvenido a uniChat</h2>
                 <div className="login-button google"
                      onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
                     <GoogleOutlined/> Inicia Sesión con Google
